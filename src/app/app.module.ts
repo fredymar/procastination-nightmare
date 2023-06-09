d1c9cfb44cf9ef91310ec5a0ab34e5030793cac0
CarlosCruzBIT <bitcarloscruz@gmail.com>
2023-06-09 17:31:37 -0500

    landing done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './routes/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { TeamComponent } from './components/team/team.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    HeroComponent,
+    AboutComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    TestimonialsComponent,
+    TeamComponent,
   ],
   imports: [
     BrowserModule,
